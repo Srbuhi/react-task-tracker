8d9074263aa45c42ce71e7bda3816065ae70faa5
Srbuhi <srbuhi.sarg@gmail.com>
2022-12-09 00:58:53 +0400

    Add new task

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ const App = () =>  {
     }
  ])
 
+  //  Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000 + 1)
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   //Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))  
@@ -35,12 +42,12 @@ const App = () =>  {
   const toggleReminder = (id) => {
     setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
   }
-
+  
   return (
     <div className="container">
       <Header />
-      <AddTask />
-      
+      <AddTask onAdd={addTask}/>
+
       {tasks.length > 0 ?
        (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : 
        ('No Tasks to show')}
