09660c821df7021927fc6d78ce8ce991309d493a
Srbuhi <srbuhi.sarg@gmail.com>
2022-12-11 01:50:45 +0400

    Toggle form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Tasks   from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () =>  {
+  const[showAddTask, setShowAddTask] = useState(false)
   const[tasks, setTasks] = useState([
     {
         id       : 1,
@@ -45,8 +46,11 @@ const App = () =>  {
   
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header
+       showAdd={showAddTask}
+       onAdd={() => setShowAddTask(!showAddTask)}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
 
       {tasks.length > 0 ?
        (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : 
